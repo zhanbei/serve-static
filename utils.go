e78f3e6c7a6324ed74cb7a5b18cf69fdd912529d
Fisher <fisher.subs@gmail.com>
2018-07-24 19:17:52 +0800

    [utils] Added #IsFileRegular(paths ... string).
    - Whether the target file exists and is a regular file.

--- utils.go
+++ utils.go
@@ -18,14 +18,17 @@ func GetTrailingNameInPath(path string) string {
 	return string(res[1])
 }
 
-// Whether the expected file exists?
-func (m *FileServer) GetFilePathFromStatics(filePath string) (bool, string) {
-	return m.GetFilePathFromStaticsAndDir("", filePath)
+// Whether the target file exists and is a regular file.
+func IsFileRegular(paths ... string) (bool, string) {
+	des := filepath.Join(paths...)
+	stat, err := os.Stat(des)
+	return !os.IsNotExist(err) && stat.Mode().IsRegular(), des
 }
 
-// Whether the expected file exists?
+func (m *FileServer) GetFilePathFromStatics(filePath string) (bool, string) {
+	return IsFileRegular(m.RootDir, filePath)
+}
+
 func (m *FileServer) GetFilePathFromStaticsAndDir(dirPath, fileName string) (bool, string) {
-	des := filepath.Join(m.RootDir, dirPath, fileName)
-	stat, err := os.Stat(des)
-	return !os.IsNotExist(err) && !stat.IsDir(), des
+	return IsFileRegular(m.RootDir, dirPath, fileName)
 }
